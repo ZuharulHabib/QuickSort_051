afd7d2769eb034f9c5a7cfd805afab2a32376556
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2023-03-30 11:38:58 +0700

    menghubungkan quick short

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -38,3 +38,60 @@ void swap(int x, int y)
 	arr[x] = arr[y];
 	arr[y] = temp;
 }
+
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)
+		return;
+
+	//Partition the list into two parts:
+	//one containing elements less that or equal to pivot
+	//Outher conntainning ellements greather that pivot
+
+	pivot = arr[low];
+
+	i = low + 1;
+	j = high;
+
+
+	while (i <= j)
+	{
+		//search for an element greather than pivot
+		while ((arr[i] <= pivot) && (i <= high))
+		{
+			i++;
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))
+		{
+			j--;
+			cmp_count++;
+		}
+		cmp_count++;
+
+
+		if (i < j) // if the grather element is on the left of the element
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j now containt the index of the last element in the sorted
+	if (low, j);
+	{
+		///move the pivot to its correct posisition in the list
+		swap(low, j);
+		mov_count++;
+	}
+	//sort the list on the left of privot using quick soert
+	q_short(low, j - 1);
+
+	//sort the on right of pivot using quick short
+	q_short(j + 1, high);
+
+
+}
